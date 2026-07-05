9583efe6c23493d6545babcd6e0d4e7eb79ff725
Rosie <rstephenson67@outlook.com>
2026-07-05 12:43:03 +0100

    added require views

---
+++ app/views/partials/header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content=""width=device-width, initial-scale="1.0">
+        <link rel="stylesheet" href="/web-portfolio/public/css/styles.css">
+
+    </head>
+    <body>
+        <header class="site-header">
+            <div class="container inner-header">
+                <!-- eventually add hamburger menu-->
+
+                <nav class="site-nav">
+                    <ul>
+                        <li><a href="homepage.php">Home</a> </li>
+                    </ul>
+                </nav>
+            </div>
+        </header>
+    </body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+
+require_once __DIR__ . '/config/config.php';
+
+$pageTitle = 'Home';
+
+require __DIR__ . '/app/views/partials/header.php';
+require __DIR__ . '/app/views/partials/footer.php';
+require __DIR__ . '/app/views/home/home_content.php';
